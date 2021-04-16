4220b724689eba1cabaa3f6fd751a0b570a915f7
johnboeira <jbiel72001@gmail.com>
2021-04-15 22:57:58 -0300

    Gerenciador Estoque v2

--- Program.cs
+++ Program.cs
@@ -6,29 +6,18 @@ using System.Threading.Tasks;
 
 namespace GestãoDeEquipamentos.ConsoleApp
 {
-    /*
-     ter um nome com no mínimo 6 caracteres;  •  Deve
-     ter um preço de aquisição;  •  Deve
-     ter um número de série;  •  Deve
-     ter uma data de fabricação;
-     
-     */
-
     class Program
     {
         static void Main(string[] args)
         {
             int opcao;
 
-            // (DateTime.Now - chamados.data).ToString("dd");
-
             //if (serie.Length != serie.Distinct().Count()) Console.WriteLine("Série já digitada");
             Inicio:
             Console.Clear();
             Console.WriteLine("Digite a opção: \n 1 Para equipamentos, \n 2 para chamados, \n 3 para sair");
             opcao = Convert.ToInt32(Console.ReadLine());
-
-
+            //equipamentos
             if (opcao == 1)
             {
                 Console.Clear();
@@ -38,7 +27,6 @@ namespace GestãoDeEquipamentos.ConsoleApp
                 double[] preco = new double[100];
                 int[] serie = new int[100];
 
-
                 Console.Clear();
 
                 while (true)
@@ -47,7 +35,7 @@ namespace GestãoDeEquipamentos.ConsoleApp
                     int opcaoEquipamento;
                     Console.WriteLine("Digite a opção: \n 1 Para criar \n 2 para editar \n 3 para excluir \n 4 para listar \n 5 para voltar");
                     opcaoEquipamento = Convert.ToInt32(Console.ReadLine());
-
+                    //criar
                     if (opcaoEquipamento == 1)
                     {
                         Console.Clear();
@@ -63,23 +51,24 @@ namespace GestãoDeEquipamentos.ConsoleApp
                         Console.WriteLine("Digite preco: ");
                         preco[contadorId] = Convert.ToDouble(Console.ReadLine());
 
-                        //Console.WriteLine("Digite data: ");
-                        //data[contadorId] = Convert.ToDateTime(Console.ReadLine());
+
+                        Console.WriteLine("Digite data: nesse formato(YYYY,MM,DD)");
+                        data[contadorId] = Convert.ToDateTime(Console.ReadLine());
 
                         contadorId++;
-                                           
-                    }
 
+                    }
+                    //editar
                     if (opcaoEquipamento == 2)
                     {
                         Console.Clear();
-                        int serieBusca;
-                        Console.WriteLine("Digite a série do produto para edição: ");
-                        serieBusca = Convert.ToInt32(Console.ReadLine());
 
-                        for (int i = 0; i < serie.Length; i++)
+                        Console.WriteLine("Digite a série do produto para edição: ");
+                        int serieBusca = Convert.ToInt32(Console.ReadLine());
+
+                        for (int i = 0; i < contadorId; i++)
                         {
-                            if (serie[i]==serieBusca)
+                            if (serie[i] == serieBusca)
                             {
                                 Console.WriteLine("Digite número série: ");
                                 serie[i] = Convert.ToInt32(Console.ReadLine());
@@ -93,26 +82,47 @@ namespace GestãoDeEquipamentos.ConsoleApp
                                 Console.WriteLine("Digite preco: ");
                                 preco[i] = Convert.ToDouble(Console.ReadLine());
 
-                                //Console.WriteLine("Digite data: ");
-                                //data[i] = Convert.ToDateTime(Console.ReadLine());
+                                Console.WriteLine("Digite data: nesse formato(YYYY,MM,DD)");
+                                data[contadorId] = Convert.ToDateTime(Console.ReadLine());
 
-                            }                         
+                            }
                         }
                         //Console.WriteLine("Valor não encontrado");
                     }
-                    
+                    //excluir
                     if (opcaoEquipamento == 3)
                     {
+                        Console.Clear();
 
+                        Console.WriteLine("Digite a série do produto para edição: ");
+                        int serieBusca = Convert.ToInt32(Console.ReadLine());
+
+                        for (int i = 0; i < contadorId; i++)
+                        {
+                            if (serie[i] == serieBusca)
+                            {
+
+                                serie[i] = 0;
+                                nome[i] = "";
+                                fabricante[i] = "";
+
+                                preco[i] = 0;
+
+
+                                data[i] = Convert.ToDateTime("0000,00,00");
+                            }
+                        }
                     }
+                    //listar
                     if (opcaoEquipamento == 4)
                     {
                         for (int i = 0; i < contadorId; i++)
                         {
-                            Console.WriteLine($"Série:{serie[i]} nome: {nome[i]} fabricante: {fabricante[i]} preco: {preco[i]}");                          
+                            Console.WriteLine($"Série:{serie[i]} nome: {nome[i]} fabricante: {fabricante[i]} preco: {preco[i]} data: {data[i]}");
                         }
                         Console.ReadLine();
                     }
+                    //voltar
                     else if (opcaoEquipamento == 5)
                     {
                         goto Inicio;
@@ -120,43 +130,116 @@ namespace GestãoDeEquipamentos.ConsoleApp
 
                 }
             }
-
+            //chamados
             if (opcao == 2)
             {
+                Console.Clear();
+                DateTime[] data = new DateTime[100];
+                string[] nome = new string[100], descricao = new string[100], equipamentos = new string[100];
+                int contadorId = 0;
+                Console.Clear();
 
+                while (true)
+                {
+                    Console.Clear();
+                    int opcaoChamado;
+                    Console.WriteLine("Digite a opção: \n 1 Para criar \n 2 para editar \n 3 para excluir \n 4 para listar \n 5 para voltar");
+                    opcaoChamado = Convert.ToInt32(Console.ReadLine());
+                    //criar 
+                    if (opcaoChamado == 1)
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("Digite título: ");
+                        nome[contadorId] = Console.ReadLine();
+
+                        Console.WriteLine("Digite descricao do produto: ");
+                        descricao[contadorId] = Console.ReadLine();
+
+                        Console.WriteLine("Digite equipamento: ");
+                        equipamentos[contadorId] = Console.ReadLine();
+
+                        Console.WriteLine("Digite data: nesse formato(YYYY,MM,DD)");
+                        data[contadorId] = Convert.ToDateTime(Console.ReadLine());
+
+                        contadorId++;
+
+                    }
+                    //editar
+                    if (opcaoChamado == 2)
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("Digite a titulo para edição: ");
+                        string titulo = Console.ReadLine();
+
+                        for (int i = 0; i < contadorId; i++)
+                        {
+                            if (nome[i] == titulo)
+                            {
+                                Console.Clear();
+
+                                Console.WriteLine("Digite título: ");
+                                nome[i] = Console.ReadLine();
+
+                                Console.WriteLine("Digite descricao do produto: ");
+                                descricao[i] = Console.ReadLine();
+
+                                Console.WriteLine("Digite equipamento: ");
+                                equipamentos[i] = Console.ReadLine();
+
+                                Console.WriteLine("Digite data: nesse formato(YYYY,MM,DD)");
+                                data[contadorId] = Convert.ToDateTime(Console.ReadLine());
+
+                            }
+                        }
+                        //Console.WriteLine("Valor não encontrado");
+                    }
+                    //excluir
+                    if (opcaoChamado == 3)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Digite a titulo para EXCLUIR: ");
+                        string titulo = Console.ReadLine();
+
+                        for (int i = 0; i < contadorId; i++)
+                        {
+                            if (nome[i] == titulo)
+                            {
+
+                                nome[i] = "";
+                                descricao[i] = "";
+                                equipamentos[i] = "";
+                                data[i] = Convert.ToDateTime("0000,00,00");
+                            }
+                        }
+                        //Console.WriteLine("Valor não encontrado");
+                    }
+                    //listar
+                    if (opcaoChamado == 4)
+                    {
+
+                        for (int i = 0; i < contadorId; i++)
+                        {
+                            string diasDif = (DateTime.Now - data[i]).ToString("dd");
+                            Console.WriteLine($"Título:{nome[i]} descrição: {descricao[i]} equipamento: {equipamentos[i]} dias em aberto: {diasDif}");
+                        }
+                        Console.ReadLine();
+                    }
+                    //voltar
+                    else if (opcaoChamado == 5)
+                    {
+                        goto Inicio;
+                    }
+                }
             }
-            else
+
+            if (opcao == 3)
             {
                 Environment.Exit(0);
             }
 
-
-
-
-
-            DateTime dataHoje = new DateTime();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
-
     }
 }
+
